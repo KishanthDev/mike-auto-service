90cf5246c05957cd235daf11369b13b2183fdf86
KishanthDev <kishanthkisna@gmail.com>
2025-04-14 12:29:10 +0530

    feat: add initial data files for business hours, certifications, gallery, reviews, and services; update next.config.js for image domains; add @heroui/card dependency

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-module.exports = nextConfig;
+const nextConfig = {
+    images: {
+      domains: ['via.placeholder.com'],
+    },
+  };
+  
+  module.exports = nextConfig;
